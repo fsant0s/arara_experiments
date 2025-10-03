915ffc7023e5905319c56fa4cc8339d498c6b04d
Fillipe Santos <fillipesantos00@gmail.com>
2025-10-03 17:40:00 -0300

    chore: update .gitignore to ignore env, cache, datasets

--- pilot/memories.py
+++
@@ -1,4 +0,0 @@
-from capabilities.memory import ListMemory, MemoryContent
-
-sequential_memory = ListMemory(name="chat_history")
-sequential_memory.add(MemoryContent(content="User's name is Shocked."))
